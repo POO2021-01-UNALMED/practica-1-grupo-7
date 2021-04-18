4f80fa806f5c85cb9ad2c8d60acdb84c6f9130e6
daruizg <79728404+daruizg@users.noreply.github.com>
2021-04-17 20:59:48 -0500

    Herencia 1.0.1

--- src/gestorAplicacion/Cliente.java
+++ src/gestorAplicacion/Cliente.java
@@ -4,6 +4,15 @@ public class Cliente extends Persona {
 	
 	// ATRIBUTOS
 	private double telefono;
+
+	// GETTERS Y SETTERS
+	public double getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(double telefono) {
+		this.telefono = telefono;
+	}
 	
 	
 	
