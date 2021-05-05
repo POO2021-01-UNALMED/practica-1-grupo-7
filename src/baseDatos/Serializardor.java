ca566b23745316b05db8a2b2d083a37962d555da
hebermudezg <hebermudezg@unal.edu.co>
2021-05-05 11:47:33 -0500

    Serializador

---
+++ src/baseDatos/Serializardor.java
@@ -0,0 +1,57 @@
+package baseDatos;
+
+// librerías
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+
+
+import matricula.Departamento;
+
+public class Serializardor {
+	
+	private static File rutaTemp = new File("src\\baseDatos\\temp");
+	
+	// Este es el método encargado de serializar las listas que estan creadas en la clase Depto
+	public static void serializar(Departamento dpto) {
+		FileOutputStream fos;
+		ObjectOutputStream oos;
+		File[] docs = rutaTemp.listFiles();
+		PrintWriter pw;
+		
+		
+		for(File file : docs) {
+			try {
+				pw = new PrintWriter(file);
+			}catch(FileNotFoundException e ) {
+				e.printStackTrace();
+			}
+		}
+		
+		for(File file : docs) {
+			if(file.getAbsolutePath().contains("asignturas")) {
+				try {
+					fos = new FileOutputStream(file);
+					oos = new ObjectOutputStream(fos);
+					oos.writeObject(dpto.getAsiganturas());	
+				}catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+			}else if(file.getAbsolutePath().contains("alumnos")) {
+				try {
+					fos = new FileOutputStream(file);
+					oos = new ObjectOutputStream(fos);
+					oos.writeObject(dpto.getAlumnos());	
+				}catch(FileNotFoundException e) {
+					e.printStackTrace();
+				}catch(IOException e) {
+					e.printStackTrace();
+				}	
+			}
+		}
+	}
+}
